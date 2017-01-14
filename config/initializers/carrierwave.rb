3322249c2067190927ccddccb010e580d85092b7
Terrence <Thenriquez@outlook.com>
2017-01-14 14:20:10 -0500

    after an log week i can finally push

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,10 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIIH4LPTX4N6WRAEA',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     ENV["aws_access_key_id"],                        # required
+    aws_secret_access_key: ENV["aws_secret_access_key"],                        # required
 
   }
-  config.fog_directory  = 'foodreviewer'                          # required
+  config.fog_directory  = ENV["fog_directory"]                        # required
 
 end
